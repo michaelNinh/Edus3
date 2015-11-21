95c8962c902a226cf8984abc5fa6ddb16c123572
michaelNinh <michaelninh@michaels-macbook-pro-2.local>
2015-11-20 19:17:38 -0800

    Initial Commit

---
+++ Edus3/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Edus3
+//
+//  Created by michael ninh on 11/20/15.
+//  Copyright © 2015 Cognitus. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
